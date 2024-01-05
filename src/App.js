cf1150fc66fcd62b0e0ad60d83f9a1621ee0b5fb
willmashipt <william.ma@shipt.com>
2024-01-05 12:27:39 -0500

    save example

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Hero from "./components/Hero";
+import Packages from "./components/Packages";
+import Footer from "./components/Footer";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <Hero />
+      <Packages />
+      <Footer />
+    </div>
+  );
+}
+export default App;
